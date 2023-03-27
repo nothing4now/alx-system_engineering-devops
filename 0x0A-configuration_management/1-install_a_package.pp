0e9e95b5bde990ebe2f07a79836bc0b59713a79e
nothing4now <yusufkolu12345@gmail.com>
2023-03-27 11:32:02 +0100

    correcting

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
 # install puppet-lint -v 2.5.0
-`
-package { 'python3-pip':
-  ensure => installed,
-}
 
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
 
